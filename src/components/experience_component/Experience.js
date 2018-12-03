3e40151d58428a973b42e91b8673178f859d5251
Gaurav Jayasawal <jgaurav6@gmail.com>
2018-12-02 22:42:11 -0500

    completed for laptop screen

--- src/components/experience_component/Experience.js
+++ src/components/experience_component/Experience.js
@@ -3,18 +3,37 @@ import ProjectCard from "./ProjectCard/ProjectCard";
 import "./experience.css";
 
 class Experience extends Component {
-  
   constructor(props) {
+    super(props);
     this.state = {
       experiences: [
-          {
-            logo: "",
-            company_name: "",
-            title: "",
-            
-          }
+        {
+          logo: require("../../main_assets/experience_logos/coding_hub.png"),
+          company_name: "Coding Hub",
+          title: "Co-founder and Vice President",
+          duties: [
+            "Supervise an agile team of 8 developers to deliver high quality applications to make hundreds of student's lives easier on campus",
+            "Spearheaded club projects by conducting weekly scrum meetings to discuss the tasks to be completed by the next sprint and assign them on Asana",
+            "Administer the GitHub repositories as the lead developer and scrum master"
+          ],
+          date: "Sep 2018 - Present",
+          location: "New York, USA"
+        },
+        {
+          logo: require("../../main_assets/experience_logos/eglu.jpg"),
+          company_name: "WiZN Systems",
+          title: "Full-stack Developer Intern",
+          duties: [
+            "Integrated APIs into an Android app to detect required network details of clients, saving the company hours of site survey time",
+            "Planned and built 2 fully responsive websites during the internship using CSS, JS to showcase the company's products and services",
+            "Developed a logger website by making REST APIs with node and express js to fetch and display server logs for rapid debugging",
+            "Stored client site analysis data on firebase databases for research to potentially optimize future deployment time by hours"
+          ],
+          date: "May 2018 - Aug 2018",
+          location: "Bangalore, IN"
+        }
       ]
-    }
+    };
   }
 
   render() {
@@ -24,7 +43,9 @@ class Experience extends Component {
           <h1>my experiences</h1>
         </div>
         <div className="project-card-list">
-          <ProjectCard />
+          {this.state.experiences.map(experience => (
+            <ProjectCard details={experience} />
+          ))}
         </div>
       </div>
     );
--- src/components/experience_component/ProjectCard/ProjectCard.js
+++ src/components/experience_component/ProjectCard/ProjectCard.js
@@ -1,42 +1,31 @@
 import React from "react";
 import "./ProjectCard.css";
-import testImg from "../../../main_assets/experience_logos/coding_hub.png";
 
 const projectCard = props => (
   <div className="projectCard">
     <div className="projectCard_row-1">
       <div className="projectCard_row-1_col1">
         <div className="row-1_col1--logo">
-          <img src={testImg} width="60px" alt="Picture" />
+          <img src={props.details.logo} width="60px" alt="Picture" />
         </div>
-        <div className="row-1_col1--company-name">CODING HUB</div>
+        <div className="row-1_col1--company-name">
+          {props.details.company_name}
+        </div>
       </div>
-      <div className="projectCard_row-1_col2">
-        Co-founder and Vice President
-      </div>
+      <div className="projectCard_row-1_col2">{props.details.title}</div>
     </div>
     <div className="row-1_bottom-divider">
       <hr />
     </div>
     <div className="projectCard_row-2">
       <ul className="projectCard_row-2_ul">
-        <li>
-          Supervise an agile team of 8 developers to deliver high quality
-          applications to make hundreds of student's lives easier on campus
-        </li>
-        <li>
-          Spearheaded club projects by conducting weekly scrum meetings to
-          discuss the tasks to be completed by the next sprint and assign them
-          on Asana
-        </li>
-        <li>
-          Administer the GitHub repositories as the lead developer and scrum
-          master
-        </li>
+        {props.details.duties.map(duty => (
+          <li>{duty}</li>
+        ))}
       </ul>
     </div>
     <div className="projectCard_row-3">
-      Sep 2018 - Present | Plattsburgh, New York
+      {props.details.date} | {props.details.location}
     </div>
   </div>
 );
